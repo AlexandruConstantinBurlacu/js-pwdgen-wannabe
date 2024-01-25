555be3fa1ecb9a946a05990733ac35aedaaea244
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-01-25 15:40:38 +0100

    aggiunta altre sezione

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// sezione nome
 let nome;
 nome = prompt("Qual è il tuo nome?")
 
 document.getElementById("first-name").innerHTML = nome ;
+
+// sezione cognome
+let cognome;
+cognome = prompt("Qual è il tuo cognome?")
+
+document.getElementById("surname").innerHTML = cognome ;
+
+// sezione colore preferito
+let colore;
+colore = prompt("Qual è il tuo colore preferito?")
+
+document.getElementById("fav-colour").innerHTML = colore ;
+
+// sezione password
+
+
+document.getElementById("fav-colour").innerHTML = colore ;
